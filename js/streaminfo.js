7005bbba148782a0f5d57f8d9bbf928ec140737f
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-05-24 01:15:00 +0900

    title to text

--- js/streaminfo.js
+++ js/streaminfo.js
@@ -4,38 +4,10 @@ $(document).ready(function() {
     function updateStreamInfo(data) {
         if (!data) return;
 
-        const nowPlaying = data.now_playing.song;
-        //const playingNext = data.playing_next.song;
-        //const songHistory = data.song_history;
-
-        // 현재 재생 중인 곡 업데이트
-        $('#cc_strinfo_song_raptureradio').html(nowPlaying.title);
-        //$('#cc_strinfo_artist').html(nowPlaying.artist);
-        //$('#cc_strinfo_album').html(nowPlaying.album);
-        //$('#cc_strinfo_art').attr('src', nowPlaying.art);
-
-        // 다음 곡 업데이트
-        //$('#cc_strinfo_next_song').html(playingNext.title);
-        //$('#cc_strinfo_next_artist').html(playingNext.artist);
-        //$('#cc_strinfo_next_album').html(playingNext.album);
-        //$('#cc_strinfo_next_art').attr('src', playingNext.art);
-
-        $('#cc_strinfo_listeners_raptureradio').html(data.listeners.current);
-
-        /* 재생 기록 업데이트
-        const historyContainer = $('#cc_strinfo_history');
-        historyContainer.empty();
-        songHistory.forEach(function(entry) {
-            const song = entry.song;
-            historyContainer.append(`
-                <li>
-                    <img src="${song.art}" alt="앨범 아트" style="width: 50px; height: 50px;">
-                    <p>제목: ${song.title}</p>
-                    <p>아티스트: ${song.artist}</p>
-                </li>
-            `);
-        });
-        */
+        const nowPlayingTitle = data.now_playing.song.text;
+        const currentListenersCount = data.listeners.current;
+        $('#cc_strinfo_song_raptureradio').html(nowPlayingTitle);
+        $('#cc_strinfo_listeners_raptureradio').html(currentListenersCount);
     }
 
     function fetchStreamInfo() {
