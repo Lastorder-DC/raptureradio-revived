f9eb8a8bb99831c1684f24cb6d862888944b2d4a
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-05-30 13:35:28 +0900

    refactor menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,29 @@
+function openPopup(url, type) {
+  const urls = {
+    radio: 'https://raptureradio.cc/',
+    guide: 'https://www.rapturearchives.org/radio/',
+    report: 'https://www.rapturearchives.org/radio/'
+  };
+
+  const sizes = {
+    radio: { height: 523, width: 614 },
+    guide: { height: 523, width: 614 },
+    report: { height: 637, width: 850 }
+  };
+
+  const finalUrl = `${urls[type]}${url}`;
+  const { height, width } = sizes[type];
+  window.open(finalUrl, `${type}Load`, `popup=true,height=${height},width=${width},left=0,top=0,noopener,noreferrer,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no`);
+}
+
 function newRadio(url) {
-	radioLoad = window.open('https://raptureradio.cc/' + url,'radioLoad','popup=true,height=523,width=614,left=0,top=0,noopener,noreferrer,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no')
+  openPopup(url, 'radio');
 }
 
 function newGuide(url) {
-	radioLoad = window.open('https://www.rapturearchives.org/radio/' + url,'guideLoad','popup=true,height=523,width=614,left=0,top=0,noopener,noreferrer,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no')
+  openPopup(url, 'guide');
 }
 
 function newReport(url) {
-	radioLoad = window.open('https://www.rapturearchives.org/radio/' + url,'reportLoad','popup=true,height=637,width=850,left=0,top=0,noopener,noreferrer,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no')
+  openPopup(url, 'report');
 }
